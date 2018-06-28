3a65128d756f1725b5fdbd3ebf33f97e245a32c4
Herbert-He24 <35063752+Herbert-He24@users.noreply.github.com>
2018-06-28 20:42:05 +0800

    Update pack.sh

--- pack.sh
+++ pack.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
 YMD=backup_$(date +%Y%m%d_%H%M)
 mv /home/user/latest.html /home/user/$YMD
 /usr/bin/python /home/user/crawl.py
 diff /home/user/latest.html /home/user/$YMD > /dev/null
 if [ $? != 0 ]; then
-        /usr/local/bin/nativefierativefier --name yahoo-$YMD  https://tw.yhaoo.com /home/user/
+        nativefier --name yahoo-$YMD  https://tw.yhaoo.com /home/user/
 fi      
 
